00f69d9199870e9ef4d6e8024730c6100fc436e8
nohsangwoo <nsgr12@naver.com>
2021-10-23 12:58:58 +0900

    seeRoom

---
+++ src/rooms/seeRoom/seeRoom.resolvers.ts
@@ -0,0 +1,26 @@
+import client from "../../client";
+import { Resolver } from "../../types";
+import { protectedResolver } from "../../users/users.utils";
+
+const resolverFn: Resolver = async (_, { id }, { loggedInUser }) => {
+  if (!loggedInUser) {
+    return null;
+  }
+
+  const result = await client.room.findFirst({
+    where: {
+      id,
+      users: {
+        some: {
+          id: loggedInUser.id
+        }
+      }
+    }
+  });
+  return result;
+};
+export default {
+  Query: {
+    seeRoom: protectedResolver(resolverFn)
+  }
+};
